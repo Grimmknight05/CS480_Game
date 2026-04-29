877187a375c85b812604fc6a386f4880eeca20e5
Joshua Henrikson <69822465+Grimmknight05@users.noreply.github.com>
2026-04-29 14:49:30 -0700

    Started Mechanic Playground, made things into prefabs Set up simple into puzzle, expanded cam shake functions

--- Assets/Scripts/Interaction/CameraShake.cs
+++ Assets/Scripts/Interaction/CameraShake.cs
@@ -1,37 +1,88 @@
 using System.Collections;
 using UnityEngine;
-// Author: Joshua Henrikson
-// Modified by: ChatGPT (April 2026)
+
 public class CameraShake : MonoBehaviour
 {
     private Vector3 originalPosition;
     private Coroutine shakeRoutine;
+
+    private bool isShaking;
+    private float currentMagnitude;
 
     private void Awake()
     {
         originalPosition = transform.localPosition;
     }
 
-    public void Shake(float duration, float magnitude)
+    public void StartShake(float magnitude, float duration = -1f)
     {
+        currentMagnitude = magnitude;
+
+        if (!isShaking)
+        {
+            isShaking = true;
+            shakeRoutine = StartCoroutine(ShakeLoop());
+        }
+
+        if (duration > 0f)
+        {
+            StartCoroutine(StopAfterDelay(duration));
+        }
+    }
+
+    public void StopShake()
+    {
+        if (!isShaking) return;
+
+        isShaking = false;
+
         if (shakeRoutine != null)
             StopCoroutine(shakeRoutine);
 
-        shakeRoutine = StartCoroutine(ShakeRoutine(duration, magnitude));
-    }
-    public void ShakeEvent()
-    {
-        Shake(0.4f, 0.25f); // default values
+        transform.localPosition = originalPosition;
+        shakeRoutine = null;
     }
 
-    private IEnumerator ShakeRoutine(float duration, float magnitude)
+    private IEnumerator ShakeLoop()
+    {
+        while (isShaking)
+        {
+            float x = Random.Range(-1f, 1f) * currentMagnitude;
+            float y = Random.Range(-1f, 1f) * currentMagnitude;
+
+            transform.localPosition = originalPosition + new Vector3(x, y, 0);
+
+            yield return null;
+        }
+    }
+    public void Shake(CamShakeConfig config)
+    {
+        if (config == null)
+        {
+            Debug.LogWarning("CameraShake: Missing config!");
+            return;
+        }
+
+        if (config.useCurve)
+        {
+            StartShakeWithCurve(config);
+        }
+        else
+        {
+            StartShake(config.magnitude, config.duration);
+        }
+    }
+    private IEnumerator ShakeWithCurveRoutine(CamShakeConfig config)
     {
         float elapsed = 0f;
 
-        while (elapsed < duration)
+        while (elapsed < config.duration)
         {
-            float x = Random.Range(-1f, 1f) * magnitude;
-            float y = Random.Range(-1f, 1f) * magnitude;
+            float t = elapsed / config.duration;
+            float strength = config.intensityOverTime.Evaluate(t) * config.magnitude;
+
+            float x = Random.Range(-1f, 1f) * strength;
+            float y = Random.Range(-1f, 1f) * strength;
 
             transform.localPosition = originalPosition + new Vector3(x, y, 0);
 
@@ -41,5 +92,33 @@ public class CameraShake : MonoBehaviour
 
         transform.localPosition = originalPosition;
         shakeRoutine = null;
+    }
+
+    private void StartShakeWithCurve(CamShakeConfig config)
+    {
+        StopShake();
+
+        isShaking = true;
+        shakeRoutine = StartCoroutine(ShakeWithCurveRoutine(config));
+    }
+    private IEnumerator StopAfterDelay(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        StopShake();
+    }
+
+    public void StartShakeEvent()
+    {
+        StartShake(0.25f);
+    }
+
+    public void StartShakeWithDuration(float duration)
+    {
+        StartShake(0.25f, duration);
+    }
+
+    public void StopShakeEvent()
+    {
+        StopShake();
     }
 }
